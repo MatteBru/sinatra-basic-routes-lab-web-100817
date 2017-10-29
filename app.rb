3d68f3abce1a3e841b2a0817d44662a3b8c5a1bc
MatteBru <mbruni616@gmail.com>
2017-10-29 14:38:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "добро пожаловать на мой очень плохой сайт"
+  end
+
+  get '/name' do
+    "My name is Матвеиы Стефанович"
+  end
+
+  get '/hometown' do
+    "My hometown is Modena, IT"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Nessun Dorma"
+  end
+
 end
